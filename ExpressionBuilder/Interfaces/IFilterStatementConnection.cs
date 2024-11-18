c0b009ea03d6a544330e79b765cd391192e1bc29
Ahmet Buğra Kösen <bugrakosen@windowslive.com>
2024-11-18 22:11:04 +0300

    fix: code cleanup apply.

--- ExpressionBuilder/Interfaces/IFilterStatementConnection.cs
+++ ExpressionBuilder/Interfaces/IFilterStatementConnection.cs
@@ -5,13 +5,13 @@
 /// </summary>
 public interface IFilterStatementConnection
 {
-	/// <summary>
-	/// Defines that the last filter statement will connect to the next one using the 'AND' logical operator.
-	/// </summary>
-	IFilter And { get; }
-	
-	/// <summary>
-	/// Defines that the last filter statement will connect to the next one using the 'OR' logical operator.
-	/// </summary>
-	IFilter Or { get; }
+    /// <summary>
+    /// Defines that the last filter statement will connect to the next one using the 'AND' logical operator.
+    /// </summary>
+    IFilter And { get; }
+
+    /// <summary>
+    /// Defines that the last filter statement will connect to the next one using the 'OR' logical operator.
+    /// </summary>
+    IFilter Or { get; }
 }
